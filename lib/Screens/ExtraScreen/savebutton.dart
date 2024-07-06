f94c5d5e4e01d0be452d5ddb73973989cd9c3067
Akhil Raj <akhilraj20801@gmail.com>
2024-07-06 16:47:46 +0530

    code commented

--- lib/Screens/ExtraScreen/savebutton.dart
+++ lib/Screens/ExtraScreen/savebutton.dart
@@ -11,6 +11,26 @@ import 'package:mere_maahi_dummy/Screens/ExtraScreen/thisProfileScreen.dart';
 import 'package:mere_maahi_dummy/Screens/Passions/PassionchipViewItem.dart';
 
 Future<void> saveProfile() async {
+  var obj = {
+    'this Profile for': selectedLabel1,
+    'religion': selectedReligion,
+    'community': selectedCommunity,
+    'relationship': relationShip,
+    'location': '$country,$state,$city',
+    'education':
+        '${graduateController.text},${univercityController.text},${stateController.text}',
+    'working': areyouworking,
+    'jobtitle': jobtitleController.text,
+    'companyname': companynameController.text,
+    'where is your company': whereisController.text,
+    'about you': aboutyouController.text,
+    'image1': image1,
+    'image2': image2,
+    'image3': image3,
+    'image4': image4,
+    'passion': Selectedpassionlables
+  };
+  print(obj);
   if (jobtitleController.text.isEmpty) {
     FirebaseFirestore.instance
         .collection('Profile')
@@ -35,24 +55,6 @@ Future<void> saveProfile() async {
     FirebaseFirestore.instance
         .collection('Profile')
         .doc(userAllDetails?.uid)
-        .set({
-      'this Profile for': selectedLabel1,
-      'religion': selectedReligion,
-      'community': selectedCommunity,
-      'relationship': relationShip,
-      'location': '$country,$state,$city',
-      'education':
-          '${graduateController.text},${univercityController.text},${stateController.text}',
-      'working': areyouworking,
-      'jobtitle': jobtitleController.text,
-      'companyname': companynameController.text,
-      'where is your company': whereisController.text,
-      'about you': aboutyouController.text,
-      'image1': image1,
-      'image2': image2,
-      'image3': image3,
-      'image4': image4,
-      'passion': Selectedpassionlables
-    });
+        .set(obj);
   }
 }
--- lib/Screens/OnboardingScreen/models/slidersection1_item_model.dart
+++ lib/Screens/OnboardingScreen/models/slidersection1_item_model.dart
@@ -1,21 +1,21 @@
-import 'package:get/get_rx/src/rx_types/rx_types.dart';
-import '../../../core/app_export.dart';
+// import 'package:get/get_rx/src/rx_types/rx_types.dart';
+// import '../../../core/app_export.dart';
 
-/// This class is used in the [slidersection1_item_widget] screen.
-class Slidersection1ItemModel {
-  Slidersection1ItemModel({
-    required this.girlOne,
-    this.premium,
-    this.id,
-  }) {
-    girlOne = girlOne ?? Rx(ImageConstant.imgGirl1);
-    premium = premium ?? Rx("Premium");
-    id = id ?? Rx("");
-  }
+// /// This class is used in the [slidersection1_item_widget] screen.
+// class Slidersection1ItemModel {
+//   Slidersection1ItemModel({
+//     required this.girlOne,
+//     this.premium,
+//     this.id,
+//   }) {
+//     girlOne = girlOne ?? Rx(ImageConstant.imgGirl1);
+//     premium = premium ?? Rx("Premium");
+//     id = id ?? Rx("");
+//   }
 
-  Rx<String>? girlOne;
+//   Rx<String>? girlOne;
 
-  Rx<String>? premium;
+//   Rx<String>? premium;
 
-  Rx<String>? id;
-}
+//   Rx<String>? id;
+// }
